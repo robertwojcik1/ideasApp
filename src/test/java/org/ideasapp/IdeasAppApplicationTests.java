9be33032c9ca0477c293aaf0edc302807de92ffc
robert <robert.wojcik.programista@gmail.com>
2025-02-08 22:52:45 +0100

    Initial commit

---
+++ src/test/java/org/ideasapp/IdeasAppApplicationTests.java
@@ -0,0 +1,13 @@
+package org.ideasapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IdeasAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
